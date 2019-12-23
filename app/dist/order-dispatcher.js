58e0d5c632aa0efb7ae72f53cefa5fc2cb4401b2
ashankaushalya97 <ashankaushalya952@gmail.com>
2019-12-23 10:21:34 +0530

    Set qtyOnHand update on order

--- app/dist/order-dispatcher.js
+++ app/dist/order-dispatcher.js
@@ -23,9 +23,11 @@ router.post('', function (req, res) {
                 res.send();
                 return;
             }
+            // connection.commit();
             console.log(req.body.orderDetails.length);
             console.log(req.body);
             console.log(req.body.orderDetails[0]);
+            //save order
             connection.query('INSERT INTO orders values(?,?,?)', [req.body.id, req.body.date, req.body.customerId], function (err, results) {
                 if (err || results.affectedRows == 0) {
                     connection.rollback();
@@ -34,6 +36,7 @@ router.post('', function (req, res) {
                     return;
                 }
                 for (var i = 0; i < req.body.orderDetails.length; i++) {
+                    //Save orderDetials
                     connection.query('INSERT INTO orderDetails VALUES (?,?,?,?)', [req.body.id, req.body.orderDetails[i].itemCode, req.body.orderDetails[i].unitPrice, req.body.orderDetails[i].qty], function (err, results) {
                         if (err || results.affectedRows == 0) {
                             connection.rollback();
@@ -41,6 +44,16 @@ router.post('', function (req, res) {
                             res.send("error in orderDetails");
                             return;
                         }
+                    });
+                    //get qtyOnHand
+                    connection.query('Update item SET qty=qty-? WHERE code=?', [req.body.orderDetails[i].qty, req.body.orderDetails[i].itemCode], function (err, results) {
+                        if (err || results.affectedRows == 0) {
+                            connection.rollback();
+                            res.status(500);
+                            res.send("error in orderDetails");
+                            return;
+                        }
+                        console.log("Item updated!");
                     });
                 }
                 connection.commit();
--- app/src/order-dispatcher.ts
+++ app/src/order-dispatcher.ts
@@ -24,10 +24,12 @@ router.post('',(req, res) => {
                 res.send();
                 return;
             }
+            // connection.commit();
             console.log(req.body.orderDetails.length);
             console.log(req.body);
             console.log(req.body.orderDetails[0]);
 
+            //save order
             connection.query('INSERT INTO orders values(?,?,?)',[req.body.id,req.body.date,req.body.customerId],function (err,results ) {
                     if (err || results.affectedRows==0){
                         connection.rollback();
@@ -38,6 +40,7 @@ router.post('',(req, res) => {
 
                 for (var i = 0; i <req.body.orderDetails.length ; i++) {
 
+                    //Save orderDetials
                     connection.query('INSERT INTO orderDetails VALUES (?,?,?,?)',[req.body.id,req.body.orderDetails[i].itemCode,req.body.orderDetails[i].unitPrice,req.body.orderDetails[i].qty],function (err,results) {
                         if (err || results.affectedRows==0){
                             connection.rollback();
@@ -46,6 +49,18 @@ router.post('',(req, res) => {
                             return;
                         }
                     });
+                    //get qtyOnHand
+                    connection.query('Update item SET qty=qty-? WHERE code=?',[req.body.orderDetails[i].qty,req.body.orderDetails[i].itemCode],function (err,results) {
+                        if (err || results.affectedRows==0){
+                            connection.rollback();
+                            res.status(500);
+                            res.send("error in orderDetails");
+                            return;
+                        }
+                        console.log("Item updated!");
+                    });
+
+
 
                 }
                 connection.commit();
