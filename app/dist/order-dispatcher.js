f4c75c807df76d93cdc2f4bdfa582c85e6b7715b
ashankaushalya97 <ashankaushalya952@gmail.com>
2019-12-23 09:20:22 +0530

    Transaction editing on order

--- app/dist/order-dispatcher.js
+++ app/dist/order-dispatcher.js
@@ -7,6 +7,9 @@ var express = require("express");
 var database_1 = __importDefault(require("./db/database"));
 var router = express.Router();
 exports.default = router;
+router.get('', function (req, res) {
+    res.send("Orders");
+});
 router.post('', function (req, res) {
     if (!req.body.id && req.body.date && req.body.customerId && typeof req.body.id == 'string' && req.body.id.trim().length > 0) {
         res.status(400);
@@ -20,21 +23,28 @@ router.post('', function (req, res) {
                 res.send();
                 return;
             }
-            connection.query('INSERT INTO orders vlaues(?,?,?)', [req.body.id, req.body.date, req.body.customerId], function (err) {
-                if (err) {
+            console.log(req.body.orderDetails.length);
+            console.log(req.body);
+            console.log(req.body.orderDetails[0]);
+            connection.query('INSERT INTO orders values(?,?,?)', [req.body.id, req.body.date, req.body.customerId], function (err, results) {
+                if (err || results.affectedRows == 0) {
                     connection.rollback();
                     res.status(500);
-                    res.send();
+                    res.send("error in orders");
                     return;
                 }
-                connection.query('INSERT INTO orderDetails VALUES (?,?,?,?)', function (err) {
-                    if (err) {
-                        connection.rollback();
-                        res.status(500);
-                        res.send();
-                        return;
-                    }
-                });
+                for (var i = 0; i < req.body.orderDetails.length; i++) {
+                    connection.query('INSERT INTO orderDetails VALUES (?,?,?,?)', [req.body.id, req.body.orderDetails[i].itemCode, req.body.orderDetails[i].unitPrice, req.body.orderDetails[i].qty], function (err, results) {
+                        if (err || results.affectedRows == 0) {
+                            connection.rollback();
+                            res.status(500);
+                            res.send("error in orderDetails");
+                            return;
+                        }
+                    });
+                }
+                res.status(201);
+                res.send("<h1>Order Detail inserted!!</h1>");
             });
         });
     });
--- app/src/order-dispatcher.ts
+++ app/src/order-dispatcher.ts
@@ -5,6 +5,11 @@ import pool from './db/database'
 const router = express.Router();
 export default router;
 
+router.get('',(req, res) => {
+   res.send("Orders");
+});
+
+
 router.post('',(req, res) => {
 
     if (!req.body.id && req.body.date && req.body.customerId && typeof req.body.id == 'string' && req.body.id.trim().length>0){
@@ -19,25 +24,36 @@ router.post('',(req, res) => {
                 res.send();
                 return;
             }
-            connection.query('INSERT INTO orders vlaues(?,?,?)',[req.body.id,req.body.date,req.body.customerId],function (err ) {
-                    if (err){
+            console.log(req.body.orderDetails.length);
+            console.log(req.body);
+            console.log(req.body.orderDetails[0]);
+
+            connection.query('INSERT INTO orders values(?,?,?)',[req.body.id,req.body.date,req.body.customerId],function (err,results ) {
+                    if (err || results.affectedRows==0){
                         connection.rollback();
                         res.status(500);
-                        res.send();
+                        res.send("error in orders");
                         return;
                     }
-                    connection.query('INSERT INTO orderDetails VALUES (?,?,?,?)',function (err) {
-                        if (err){
+
+                for (var i = 0; i <req.body.orderDetails.length ; i++) {
+
+                    connection.query('INSERT INTO orderDetails VALUES (?,?,?,?)',[req.body.id,req.body.orderDetails[i].itemCode,req.body.orderDetails[i].unitPrice,req.body.orderDetails[i].qty],function (err,results) {
+                        if (err || results.affectedRows==0){
                             connection.rollback();
                             res.status(500);
-                            res.send();
+                            res.send("error in orderDetails");
                             return;
                         }
                     });
 
-            })
+                }
+                res.status(201);
+                res.send("<h1>Order Detail inserted!!</h1>");
 
-        })
+            });
+
+        });
 
 
     })
